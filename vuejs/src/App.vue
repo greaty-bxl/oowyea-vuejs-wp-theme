0fcd614ff9b549235a52b171a1e22deac6250eba
Djhé <i@greaty.be>
2020-06-21 16:45:15 +0200

    Update App.vue

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -137,6 +137,7 @@ export default {
 
 <style>
 #app {
+  position: fixed;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;

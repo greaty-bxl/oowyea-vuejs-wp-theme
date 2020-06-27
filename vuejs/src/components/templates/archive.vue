7a49e03e7c198e2b6c72366e62a0aab6e6b2dc96
Djhé <i@greaty.be>
2020-06-27 23:31:07 +0200

    Create archive.vue

---
+++ vuejs/src/components/templates/archive.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="section-wrap">
+		<div class="clear"></div>
+		<h1 v-html="title"></h1>
+		<p>Archive template</p>
+		
+		<div class="clear"></div>
+	</div>
+</template>
+
+<script>
+	export default {
+		components: {
+			
+		},
+		props: {
+			'title': String,
+			'posts' : Array,
+		},
+		mounted (){
+			this.$emit('template_mounted')
+		}
+	}
+</script>

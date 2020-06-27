1c231e32d3f79adc0f2d5413343daa28568bfa81
Djhé <i@greaty.be>
2020-06-27 23:31:04 +0200

    Create 404.vue

---
+++ vuejs/src/components/templates/404.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="section-wrap">
+		<div class="clear"></div>
+		<h1>404</h1>
+		<p>404 template</p>
+		
+		<div class="clear"></div>
+	</div>
+</template>
+
+<script>
+	export default {
+		components: {
+			
+		},
+		props: {
+			'post' : Object
+		},
+		mounted (){
+			this.$emit('template_mounted')
+		}
+	}
+</script>

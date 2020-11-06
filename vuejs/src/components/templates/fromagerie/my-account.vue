dd74fe5117c5aa617a443b7d04b1ccde1aced446
Djhé <i@greaty.be>
2020-11-06 14:44:20 +0100

    Update my-account.vue

--- vuejs/src/components/templates/fromagerie/my-account.vue
+++ vuejs/src/components/templates/fromagerie/my-account.vue
@@ -30,12 +30,20 @@ export default {
 
 
 	mounted(){
-	
+		
+		$ = this.$
+
 		this.$emit('template_mounted', this);
-		console.log(this.wp);
+		//console.log(this.wp);
 		smart_fonts({
 			'.title-compte' : 25,
 		})
+
+		//resolve login issue 'add to json'
+		var referer_val = $('[name="_wp_http_referer"]').prop('value')
+		referer_val = referer_val.replace('?add_to_json=1', '')
+		referer_val = referer_val.replace('&add_to_json=1', '')
+		$('[name="_wp_http_referer"]').val(referer_val)
 
 	},
 

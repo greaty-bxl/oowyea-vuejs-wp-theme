f0ff5abe459eda28a06ccc397fcb6b5a97b7debc
Djhé <i@greaty.be>
2020-11-19 17:19:39 +0100

    Noty

--- vuejs/src/components/templates/single-product.vue
+++ vuejs/src/components/templates/single-product.vue
@@ -91,14 +91,14 @@
 
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400&display=swap');
 
-	.noty_theme__semanticui.noty_type__success{
+	/*.noty_theme__semanticui.noty_type__success{
 
 		background-color: #70625B !important;
 		color: white !important;
 	}
 	.noty_theme__semanticui.noty_type__success a{
 		color: white !important;
-	}
+	}*/
 
 	#header{
 

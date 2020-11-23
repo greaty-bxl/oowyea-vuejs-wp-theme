ab400f6c60adebd571a317817b021981f60b46d9
Djhé <i@greaty.be>
2020-11-23 14:17:34 +0100

    checkout calendar

--- libs/woocommerce.php
+++ libs/woocommerce.php
@@ -1,7 +1,9 @@
 <?php
+
 include 'woocommerce/remove-scripts.php';
 include 'woocommerce/hierarchy.php';
 include 'woocommerce/shortcode.php';
 include 'woocommerce/return-to-shop.php';
 include 'woocommerce/ajax_forms.php';
 include 'woocommerce/product.php';
+include 'woocommerce/calendar.php';

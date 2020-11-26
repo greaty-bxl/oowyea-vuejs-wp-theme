d70481b8a08ec0aeaf1e1339d6f4e881adec9db7
Djhé <i@greaty.be>
2020-11-26 13:37:52 +0100

    Update calendar.php

--- libs/woocommerce/calendar.php
+++ libs/woocommerce/calendar.php
@@ -40,12 +40,12 @@ function wc_validate_calendar_hour_field( $fields, $errors ){
 
 	if( empty( $_POST[ 'pick_date' ] ) )
 	{
-		$errors->add( 'validation', "Merci de choisir une date d'enlèvement." );
+		$errors->add( 'validation', "Merci de choisir une date de retrait en magasin." );
 	}
 
 	if( empty( $_POST[ 'pick_hour' ] ) )
 	{
-		$errors->add( 'validation', "Merci de choisir une heure d'enlèvement." );
+		$errors->add( 'validation', "Merci de choisir une heure de retrait en magasin." );
 	}
 
     $today = date("Y-m-d");
@@ -59,23 +59,23 @@ function wc_validate_calendar_hour_field( $fields, $errors ){
     if( $_POST['pick_date'] <= $today )
     {
         //echo "Date trop petite";
-        $errors->add( 'validation', "Veuillez choisir une date à partir de demain." );
+        $errors->add( 'validation', "Veuillez choisir une date à partir du lendemain." );
     }
 
     //If hour is more than 19h30 and date tomorrow
     if( $_POST['pick_date'] <= $tomorrow && $now >= $max_H )
     {
         //echo "Trop tard pour demain";
-        $errors->add( 'validation', "Si vous commandez après 19h30, votre commande peut être prête pour après demain. Merci de choisir une autre date." );
+        $errors->add( 'validation', "Si vous commandez après 19h30, votre commande peut être prête pour sur le lendemain. Merci de choisir une autre date." );
     }
 
     //No order on sunday
     if( $is_sunday )
     {
         //echo "Pas de commande le dimanche";
-        $errors->add( 'validation', "Nous ne préparons pas de commande le dimanche." );
+        $errors->add( 'validation', "Nous ne préparons pas de commande le dimanche car nous sommes fermé." );
     }
-    
+
 }
 
 add_action( 'woocommerce_after_checkout_validation', 'wc_validate_calendar_hour_field', 10, 2);

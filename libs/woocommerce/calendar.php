0d6f4239fe5d6fc196397f100646a0f963141020
Djhé <i@greaty.be>
2020-12-04 14:38:15 +0100

    Update calendar.php

--- libs/woocommerce/calendar.php
+++ libs/woocommerce/calendar.php
@@ -108,19 +108,50 @@ function wc_display_admin_calendar($order){
 
 add_action( 'woocommerce_admin_order_data_after_billing_address', 'wc_display_admin_calendar', 10, 1 );
 
+
 /**
  * Add a custom field (in an order) to the emails
  */
-function wc_email_calendar( $fields, $sent_to_admin, $order ) {
+/*function wc_email_calendar( $fields, $sent_to_admin, $order ) {
     $fields['pick_date'] = array(
-        'label' => __( "Date d'enlèvement" ),
-        'value' => get_post_meta( $order->id, 'pick_date', true ) . ' à ' . get_post_meta( $order->id, 'pick_hour', true ),
+        'label' => "<span style='font-size: 18px;'>" . __( "Date d'enlèvement" ) . "</span>",
+        'value' => "<span style='font-size: 18px;'>" . get_post_meta( $order->id, 'pick_date', true ) . ' à ' . get_post_meta( $order->id, 'pick_hour', true ) . "<br/><br/></span>",
     );
     return $fields;
 }
 
-add_filter( 'woocommerce_email_order_meta_fields', 'wc_email_calendar', 10, 3 );
+add_filter( 'woocommerce_email_order_meta_fields', 'wc_email_calendar', 10, 3 );*/
 
+
+function add_tracking_number_to_order_email( $order, $sent_to_admin, $plain_text, $email )
+{
+    //if( empty( $has_discount ) && empty( $po_number ) ) return; // Exit if empty
+    $base_color = get_option( 'woocommerce_email_base_color' );
+    // CSS style
+    $styles = '<style>
+        table.pick_date{width: 100%; font-family: \'Helvetica Neue\', Helvetica, Roboto, Arial, sans-serif;
+            color: '.$base_color.'; border: 1px solid #e4e4e4; margin-bottom:8px;}
+        table.pick_date th, table.tracking-info td{text-align: left; border-top-width: 4px;
+            color: '.$base_color.'; border: 1px solid #e4e4e4; padding: 12px; width:50%;}
+        table.pick_date td{text-align: left; border-top-width: 4px; color: '.$base_color.'; border: 1px solid #e4e4e4; padding: 12px; width:50%;}
+    </style>';
+
+    // HTML Structure
+    $html_output = '<h2>'.__( "Date d'enlèvement" ).'</h2>
+    <table class="pick_date" cellspacing="0" cellpadding="6">';
+
+    
+    $html_output .= '<tr class="sales-rep">
+            <td>' . get_post_meta( $order->id, 'pick_date', true ) . ' à ' . get_post_meta( $order->id, 'pick_hour', true ) . '</td>
+        </tr>';
+    
+
+    $html_output .= '</table><br>'; // HTML (end)
+
+    // Output styles CSS + HTML
+    echo $styles . $html_output;
+}
+add_action( 'woocommerce_email_after_order_table', 'add_tracking_number_to_order_email', 10, 4 );
 
 /**
  * Thank you order display

69bcc9a15c49d22ebbd50bfaa02286e9f1abfd51
Djhé <i@greaty.be>
2021-01-05 10:12:40 +0100

    language as slug

--- libs/pll_langue/menus_pll.php
+++ libs/pll_langue/menus_pll.php
@@ -3,7 +3,10 @@
 function get_menus_pll()
 {	
 	// $args =
-	$pll = pll_the_languages( array('echo' => 0));
+	$pll = pll_the_languages( array(
+		'echo' => 0,
+		'display_names_as' => 'slug'
+	));
 
 	wp_vue_add_var('pll', $pll);
 }

649b06efa54f43132b06efd29ef98b3f16e1b123
Djhé <i@greaty.be>
2021-01-06 08:19:11 +0100

    Update filter-product-by-page.php

--- libs/filtre_woocommerce/filter-product-by-page.php
+++ libs/filtre_woocommerce/filter-product-by-page.php
@@ -2,108 +2,51 @@
 
 	function global_funtion_filter( $query ) {
 
-	
-		if ( $query->query['post_type'] == 'product' && !is_admin()  && !$query->is_single  && is_page('boutique')  )
+		if ( $query->query['post_type'] == 'product' && !is_admin()  && !$query->is_single && $_GET['filter-taxonomy'] )
 		{	
-
 			if ( $_GET['filter-taxonomy'] ) 
 			{	
-		
 				$taxonomies = $_GET;
 
 				$category = $_GET['product_cat'];
-
 			}
 			else if ( $_POST['filter-taxonomy'] )
 			{
 				$taxonomies = $_POST;
-
 			}
-			else{ 
+			else
+			{ 
+				return;
 			}
 
-			unset($taxonomies['filter-taxonomy']);
-			unset($taxonomies['product_cat']);
+			unset( $taxonomies['filter-taxonomy'] );			
 
-		
-			$arrayjquery = array();
+			if ( count( $taxonomies ) > 0 ) 
+			{
+				$tax_query = array( 'relation' => 'AND' );
 
-		
-			
-			// $query->set( 'tax_query', array(
+				foreach ($taxonomies as $name => $terms) 
+				{
+					if( !is_array( $terms ) )
+					{
+						$terms = array( $terms ); 
+					}
 
-			// 	'relation' => 'AND',
+					$tax_query[] = array(
+						'taxonomy'     => $name,
+						'field' => 'slug',
+						'terms' =>  $terms,
+						'include_children' => true, 
+						'operator' => 'IN'
+					);
+				}
 
-			//            array(
-			//                'taxonomy'     => 'product_cat',
-			//                'field' => 'slug',
-			//                'terms' =>  $category,
-			//                'include_children' => true, 
-			//                'operator' => 'IN' 
-			//                ),
-
-			//             ) );
-
-			// foreach ($taxonomies as $key => $value) {
-
-			// 	    $query->set( 'tax_query', array(
-
-				    
-   //  					'relation' => 'AND',
-
-   //  			           array(
-
-   //  			               'taxonomy'     => 'product_cat',
-   //  			               'field' => 'slug',
-   //  			               'terms' =>  $category,
-   //  			               'include_children' => true, 
-   //  			               'operator' => 'IN' 
-
-   //  			               ),
-
-			//                array(
-
-			//                    'taxonomy'  => $key,
-			//                    'field' => 'slug',
-			//                    'terms' =>   $value,
-			//                    'include_children' => true, 
-			//                    'operator' => 'IN' 	
-
-			//                    )
-
-			// 	     ) 
-			// 	);
-
-
-			// 	};
-
+				$query->set( 'tax_query', $tax_query );
+			}
 		}
 				
 	};
 
 	add_action( 'pre_get_posts', 'global_funtion_filter' ); // used on load
 
-
-	function gtr_get_les_cafes( $query  ) {
-
-
-		if ( is_page('boutique')) {
-
-				$posts_1 = get_posts( array('post_type' => 'product'));
-
-				$args = array();
-														
-				$products = wc_get_products( $args );
-
-				$generale = apply_filters( 'posts_results', $posts_1 ) ;
-
-				wp_vue_add_var('generale', $generale);
-
-			}
-
-	};
-	add_action( 'wp', 'gtr_get_les_cafes' );
-	
-	add_action( 'vue_vars', 'gtr_get_les_cafes' ); 
-
-	?>
+?>

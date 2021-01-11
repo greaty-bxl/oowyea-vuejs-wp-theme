65940c4cde42115cd4ec984f73b822a5a4a9f20b
Djhé <i@greaty.be>
2021-01-11 10:57:41 +0100

    filters

--- libs/woocommerce/taxonomies-filter.php
+++ libs/woocommerce/taxonomies-filter.php
@@ -89,16 +89,20 @@ function owy_get_product_cat_filters_lists_with_relations() {
 
 		$include_filters = get_field('include_in_filtering', 'option');
 		$shop_filter = array();
-		foreach ($include_filters as $key => $filter) 
+
+		if( is_array($include_filters) )
 		{
-			$shop_filter[] = array(
-				'label' => get_taxonomy( $filter['tax_filter_in'] )->label,
-				'slug' => $filter['tax_filter_in'],
-				'terms' => get_terms( array('taxonomy'=> $filter['tax_filter_in'] ) )
-			);
+			foreach ($include_filters as $key => $filter) 
+			{
+				$shop_filter[] = array(
+					'label' => get_taxonomy( $filter['tax_filter_in'] )->label,
+					'slug' => $filter['tax_filter_in'],
+					'terms' => get_terms( array('taxonomy'=> $filter['tax_filter_in'] ) )
+				);
+			}
+			wp_vue_add_var('shop_filter', $shop_filter );
+			wp_vue_add_var('product_cat_filters_extended', $terms );	
 		}
-		wp_vue_add_var('shop_filter', $shop_filter );
-		wp_vue_add_var('product_cat_filters_extended', $terms );
 	}
 }
 

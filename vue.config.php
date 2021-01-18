f9c8a07cc10bcb2b9321216df94704c2f7b70125
Djhé <i@greaty.be>
2021-01-18 11:29:58 +0100

    Update vue.config.php

--- vue.config.php
+++ vue.config.php
@@ -93,7 +93,6 @@ function is_hierarchical_template($name)
     }
     get_vue_templates(GREATY_TEMPLATE_PATH.'/vuejs/src/components/templates');
 
-    define( 'WP_DEBUG', true );
     /*echo "<pre>";
     print_r( $vue_hierarchy );
     echo "<pre>";

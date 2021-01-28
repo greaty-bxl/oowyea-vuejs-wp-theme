fb655bac72315e830c7acbd34e82d98fd8552061
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-01-28 14:00:56 +0100

    Update product-filter.vue

--- vuejs/src/components/organisms/product-filter.vue
+++ vuejs/src/components/organisms/product-filter.vue
@@ -395,7 +395,11 @@
 	@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap');
 
 	.filters::-webkit-scrollbar-thumb{
-		background: #e1e1e1;
+		background: #e1e1e1 !important;
+	}
+
+	.filters::-webkit-scrollbar-thumb, ::-webkit-scrollbar-thumb:hover{
+		background: #e1e1e1 !important;
 	}
 
 	@media only screen and (min-width: 1101px){

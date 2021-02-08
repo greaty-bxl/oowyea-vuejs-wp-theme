ba0b79e57370171aacae9cc88057d3d1aeb27645
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-02-08 13:02:44 +0100

    Update woocommerce.php

--- libs/woocommerce.php
+++ libs/woocommerce.php
@@ -17,6 +17,9 @@ include 'woocommerce/count.php';
 include 'woocommerce/cart-global.php';
 include 'woocommerce/currency-format.php';
 include 'woocommerce/account-global.php';
+include 'woocommerce/checkout.php';
+
+
 
 
 

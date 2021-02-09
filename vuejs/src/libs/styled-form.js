795b08a2c6599d23b32d581fd62d69d47dba254a
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-02-09 12:58:07 +0100

    Update styled-form.js

--- vuejs/src/libs/styled-form.js
+++ vuejs/src/libs/styled-form.js
@@ -5,33 +5,57 @@ export default function ( selector ){
 	$(selector).each( (index, el) => {
 		
 				$(el).parent().prev().find('label').css('display', 'none');
+
+
 					$(el).parent().parent().prev().find('label').css('display', 'none');
 
 
 				var placehold 
 
+
 				if ($(el).parents().hasClass('acfe-form') ) {
 
+					console.log('acfe-form');
+
+					placehold = ""
+
+					console.log($(el));
+
 					placehold = $(el).parent().parent().parent().find('label').text().replace('*', '\n');
-					$(el).parent().parent().parent().find('label').text(placehold)
 					$(el).parent().parent().parent().find('label').addClass('third-child')
+
+					$(el).attr('placeholder', placehold);
 
 					if ( $(el).is("textarea") ) {
 
+						placehold = ""
+
 						placehold = $(el).parent().parent().find('label').text().replace('*', '\n');
+						// $(el).parent().parent().find('label').css('display', 'none');
 						$(el).parent().parent().find('label').text(placehold)
 						$(el).parent().parent().find('label').addClass('second-child')
+
+						$(el).attr('placeholder', placehold);
 					}
 
 				}
 				else{
 
+					console.log('not acf');
+
+					placehold = ""
+
 					placehold = $(el).parent().parent().find('label').text().replace('*', '\n');
+					$(el).parent().parent().find('label').css('display', 'none');
 					$(el).parent().parent().find('label').text(placehold)
 					$(el).parent().parent().find('label').addClass('second-child')
+
+					$(el).attr('placeholder', placehold);
 				}
 
-			$(el).attr('placeholder', placehold);
+			
+
+			console.log(placehold);
 
 	});
 
@@ -39,24 +63,62 @@ export default function ( selector ){
 
 		$(selector).on("change paste keyup", function() {
 
-			$(this).addClass('paddingTop-animation')
-
 			if ($(this).parents().hasClass('acfe-form') ){
 
-				console.log('qfpo');
+				$(this).addClass('paddingTop-animation-acf')
+
+				if ( $(this).is("textarea") ){
+
+					$(this).parent().parent().find('label').addClass('appear-label-acf')
+				}
+				else{
+
+					$(this).parent().parent().parent().find('label').addClass('appear-label-acf')
+				}
 
 			}
 			else{
 
-				$(this).parent().parent().find('label').addClass('appeare-label')
+				$(this).addClass('paddingTop-animation')
+				
+				$(this).parent().parent().find('label').addClass('appear-label')
 			}
 
 			if ($(this).val() === '') {
 
-				$(this).removeClass('paddingTop-animation')
-				$(this).parent().parent().find('label').removeClass('appeare-label')
-				
-				console.log('vide');
+
+				if ($(this).parents().hasClass('acfe-form') ){
+
+					if ( $(this).is("textarea") ){
+
+						$(this).parent().parent().find('label').removeClass('appear-label-acf')
+					}
+					else{
+
+						$(this).parent().parent().parent().find('label').removeClass('appear-label-acf')
+
+					}
+
+
+				}
+
+				else{
+
+					if ( $(this).is("textarea") ){
+
+						$(this).parent().parent().find('label').removeClass('appear-label')
+					}
+					else{
+
+						$(this).parent().parent().parent().find('label').removeClass('appear-label')
+
+					}
+
+				}
+
+					$(this).removeClass('paddingTop-animation')
+					$(this).removeClass('paddingTop-animation-acf')			
+
 			}
 			else{
 

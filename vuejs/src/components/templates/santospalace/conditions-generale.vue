b7c17b53e1b37feaaacbad729612108c8360f45f
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-02-09 14:14:16 +0100

    Update conditions-generale.vue

--- vuejs/src/components/templates/santospalace/conditions-generale.vue
+++ vuejs/src/components/templates/santospalace/conditions-generale.vue
@@ -1,13 +1,9 @@
 <template>
 
-	<div>
-	
-	<div class="parent-text">
-		
-		<div v-html="post.post_content"> 
-	</div>
 
-	</div>	
+	<div class="global-css-pages-legale">
+		<div v-html="post.post_content"> 
+		</div>
 	</div>
 
 </template>
@@ -37,83 +33,7 @@ export default {
 
 <style>
 
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400&display=swap');
 
-
-	#conditions-generales-de-vente {
-
-	background-color: white;
-	}
-
-	.parent-text{
-
-		text-align: left;
-		width: 70%;
-		margin: 150px auto;
-
-	}
-	.parent-text h2{
-
-		color: #888320 !important;
-		font-size: 30px;
-		
-
-	}
-
-	.parent-text h1{
-
-		color: #888320 !important;
-		font-size: 40px !important;
-
-	}
-
-	.parent-text h3{
-
-		color: #888320 !important;
-
-	}
-
-	.parent-text p{
-
-		font-size: 17px;
-		line-height: 170%;
-
-	}
-
-
-	@media screen and (max-width: 600px){
-
-		.parent-text{
-
-			text-align: left;
-			width: calc(100% - 60px);
-			margin: 150px 30px;
-
-		}
-	
-	}
-
-
-	@media screen and (max-width: 1125px) and (min-width: 600px){
-
-
-			.parent-text{
-
-			text-align: left;
-			width: calc(100% - 140px);
-			margin: 150px 70px;
-
-		}
-
-	}
-
-	
-
-	@media screen and (max-width: 4000px) and (min-width: 1700px){
-
-	
-
-	}
 
 
 </style>

562307cfbddb9904587beab840e8a7d6abbc1500
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-02-09 12:59:27 +0100

    Create mention-legal.vue

---
+++ vuejs/src/components/templates/santospalace/mention-legal.vue
@@ -0,0 +1,33 @@
+<template>
+
+	<div>
+	
+	<div class="parent-text">
+		
+		<div v-html="post.post_content"> 
+	</div>
+
+	</div>	
+	</div>
+
+</template>
+
+<script>
+
+export default {
+	components: {
+
+	},
+	props: {
+		'post' : Object
+	},
+
+	mounted(){
+	
+		this.$emit('template_mounted', this);
+		console.log(this.wp);
+
+	},
+
+}
+

bfb314ae0467ef8cb95eaa7ccf4902b5b6ed7c74
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-02-10 09:43:48 +0100

    Update page.vue

--- vuejs/src/components/templates/page.vue
+++ vuejs/src/components/templates/page.vue
@@ -1,8 +1,14 @@
 <template>
 	<div class="section-wrap">
 		<div class="clear"></div>
-		<h1 v-html="post.post_title"></h1>
-		<div class="wp-content" v-html="post.post_content"></div>
+
+	
+		<div class="wp-content vue-page ">
+
+			<h1 v-html="post.post_title"></h1>
+			<div v-html="post.post_content"></div>
+		
+		</div>
 		<div class="clear"></div>
 	</div>
 </template>
@@ -16,7 +22,49 @@
 			'post' : Object
 		},
 		mounted (){
-			//this.$emit('template_mounted')
+	
 		}
 	}
 </script>
+
+
+
+
+<style scoped >
+	
+	.vue-page {
+
+		text-align: left;
+		width: 70% !important;
+		margin: 150px auto;
+
+	}
+
+
+	@media screen and (max-width: 600px){
+
+		.vue-page{
+
+			text-align: left;
+			width: calc(100% - 60px) !important;
+			margin: 150px 30px;
+
+		}
+
+	}
+
+
+	@media screen and (max-width: 1125px) and (min-width: 600px){
+
+			.vue-page{
+
+			text-align: left;
+			width: calc(100% - 140px) !important;
+			margin: 150px 70px;
+
+		}
+
+	}
+
+
+</style>

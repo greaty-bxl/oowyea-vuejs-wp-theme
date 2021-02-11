205cddaf99fa0a66f8a5256fb76fa1b87ea72b97
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-02-11 15:18:36 +0100

    Update styled-form.js

--- vuejs/src/libs/styled-form.js
+++ vuejs/src/libs/styled-form.js
@@ -4,8 +4,6 @@ export default function ( selector ){
 
 	$(selector).each( (index, el) => {
 
-		// $(el).find('label').hide();
-
 		let input = $(el)
 		let id = input.prop('id')
 		let label = $(el).parents('form').find('label[for="'+id+'"]')
@@ -23,83 +21,6 @@ export default function ( selector ){
 		
 		});
 		
-		/*$(el).parent().prev().find('label').css('display', 'none');
-
-		$(el).parent().parent().prev().find('label').css('display', 'none');
-
-
-		var placehold 
-
-
-
-		if ($(el).parents().hasClass('acfe-form') ) {
-
-			console.log('acfe-form');
-
-			placehold = ""
-
-			console.log($(el));
-
-			placehold = $(el).parent().parent().parent().find('label').text().replace('*', '');
-			$(el).parent().parent().parent().find('label').addClass('third-child')
-
-			$(el).attr('placeholder', placehold);
-
-			if ( $(el).is("textarea") ) {
-
-				placehold = ""
-
-				placehold = $(el).parent().parent().find('label').text().replace('*', '');
-				// $(el).parent().parent().find('label').css('display', 'none');
-				$(el).parent().parent().find('label').text(placehold)
-				$(el).parent().parent().find('label').addClass('second-child')
-
-				$(el).attr('placeholder', placehold);
-			}
-
-		}
-		else{
-
-			console.log('not acf');
-
-			placehold = ""
-
-			let placehold_el = $(el).parent().parent().find('label')
-
-			if( placehold_el.length == 0 )
-			{
-				placehold_el = $(el).parent().find('label')
-			}
-			// console.log(placehold_el.text());
-
-			if( placehold_el.length == 1 )
-			{
-
-				placehold = " ";
-
-				placehold = $(el).parent().parent().find('label').text()
-
-				console.log($(el));
-
-				console.log($(el).parent().parent().find('label'));
-
-				$(el).parent().find('label').css('display', 'none');
-
-				// $(el).parent().parent().find('label').css('display', 'none');
-				// $(el).parent().parent().find('label').text(placehold)
-
-				$(el).parent().parent().find('label').addClass('second-child')
-
-				$(el).attr('placeholder', placehold);
-			}
-			
-			
-		}*/
-
-			
-
-		// console.log(placehold);
-
 	});
 
 	function check_state(input, label) {

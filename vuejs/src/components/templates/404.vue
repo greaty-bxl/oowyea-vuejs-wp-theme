afde6fa6575efb7ac77ff6c3ccafd0a129547d3b
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-02-26 16:01:12 +0100

    Update 404.vue

--- vuejs/src/components/templates/404.vue
+++ vuejs/src/components/templates/404.vue
@@ -1,15 +1,11 @@
 <template>
 	<div class="section-wrap">
 		<div class="clear"></div>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<h1>404</h1>
-		<p>...</p>
+		<div class="div-404">
+			
+			<h1>404</h1>
+
+		</div>
 		
 		<div class="clear"></div>
 	</div>
@@ -28,3 +24,28 @@
 		}
 	}
 </script>
+
+
+<style type="text/css">
+	
+	.div-404{
+
+		background-color: #888320;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-content: center;
+
+	}
+
+	.div-404 h1{
+		margin-top: auto;
+		margin-bottom: auto;
+		color: white;
+		font-size: 100px;
+
+	}
+
+
+
+</style>

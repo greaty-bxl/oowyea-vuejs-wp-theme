7165b8746ec77c09d23ab271589c1625e49b41ad
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-03-03 11:09:51 +0100

    acf

--- acfe-php/group_603f4676e6e63.php
+++ acfe-php/group_603f4676e6e63.php
@@ -3,13 +3,13 @@
 if( function_exists('acf_add_local_field_group') ):
 
 acf_add_local_field_group(array(
-	'key' => 'group_603cb0a5e37ba',
-	'title' => 'Téléchargement',
+	'key' => 'group_603f4676e6e63',
+	'title' => 'downloaden',
 	'fields' => array(
 		array(
-			'key' => 'field_603e42637e235',
-			'label' => 'fichier a télécharger',
-			'name' => 'fichier_a_telecharger',
+			'key' => 'field_603f468de16fa',
+			'label' => 'bestand om te downloaden',
+			'name' => 'bestand_om_te_downloaden',
 			'type' => 'repeater',
 			'instructions' => '',
 			'required' => 0,
@@ -22,14 +22,14 @@ acf_add_local_field_group(array(
 			'acfe_repeater_stylised_button' => 0,
 			'collapsed' => '',
 			'min' => 0,
-			'max' => 3,
+			'max' => 0,
 			'layout' => 'table',
 			'button_label' => '',
 			'sub_fields' => array(
 				array(
-					'key' => 'field_603e42c57e236',
-					'label' => 'Text button',
-					'name' => 'text_button',
+					'key' => 'field_603f47f8d5db6',
+					'label' => 'Tekst knop',
+					'name' => 'tekst_knop',
 					'type' => 'select',
 					'instructions' => '',
 					'required' => 0,
@@ -40,11 +40,11 @@ acf_add_local_field_group(array(
 						'id' => '',
 					),
 					'choices' => array(
-						'Manuel d’utilisation' => 'Manuel d’utilisation',
-						'Service manuel' => 'Service manuel',
-						'Fiche de spécification' => 'Fiche de spécification',
+						'Specificatiefiche' => 'Specificatiefiche',
+						'Gebruiksaanwijzing' => 'Gebruiksaanwijzing',
+						'Algemene catalogus' => 'Algemene catalogus',
 					),
-					'default_value' => false,
+					'default_value' => 'Specificatiefiche',
 					'allow_null' => 0,
 					'multiple' => 0,
 					'ui' => 0,
@@ -53,7 +53,7 @@ acf_add_local_field_group(array(
 					'placeholder' => '',
 				),
 				array(
-					'key' => 'field_603e43397e237',
+					'key' => 'field_603f4844d5db7',
 					'label' => 'pdf',
 					'name' => 'pdf',
 					'type' => 'file',
@@ -80,14 +80,14 @@ acf_add_local_field_group(array(
 			array(
 				'param' => 'post_taxonomy',
 				'operator' => '==',
-				'value' => 'language:fr',
+				'value' => 'language:nl',
 			),
 		),
 		array(
 			array(
 				'param' => 'post_taxonomy',
 				'operator' => '==',
-				'value' => 'product_cat:lave-vaisselle-2',
+				'value' => 'product_cat:washmachine',
 			),
 		),
 	),
@@ -107,7 +107,7 @@ acf_add_local_field_group(array(
 	'acfe_form' => 0,
 	'acfe_meta' => '',
 	'acfe_note' => '',
-	'modified' => 1614761361,
+	'modified' => 1614761417,
 ));
 
 endif;

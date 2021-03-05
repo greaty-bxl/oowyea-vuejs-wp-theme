ce51399a6925d45007de43ac3ba108c3ed3bb5e1
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-03-05 13:01:53 +0100

    Update single-product.vue

--- vuejs/src/components/templates/single-product.vue
+++ vuejs/src/components/templates/single-product.vue
@@ -66,22 +66,32 @@
 								<p v-html="post.post_content" class="descriptif"></p>
 							</div>
 
-								<div v-if="post.acf_fields.fichier_a_telecharger != undefined" class="download-parent">
-									<div style="padding-bottom: 30px">
-										<div  v-for="(item, key)  in post.acf_fields.fichier_a_telecharger" :key="key" >
-											<a data-trigger="download-pdf" v-if="item.pdf != false" class="download " :href="item.pdf" target="_blank" v-html="item.text_button"  >
-											</a>
+								<div v-if="post.acf_fields.fichier_a_telecharger != false" >
+									<div class="download-parent">
+										<div style="padding-bottom: 30px">
+											<div  v-for="(item, key)  in post.acf_fields.fichier_a_telecharger" :key="key" >
+												<a data-trigger="download-pdf" v-if="item.pdf != false" class="download " :href="item.pdf" target="_blank" v-html="item.text_button"  >
+												</a>
+											</div>
 										</div>
 									</div>
 								</div>
 
-								<div  v-if="post.acf_fields.bestand_om_te_downloaden != undefined" class="download-parent">
-									<div style="padding-bottom: 30px">
-										<div  v-for="(item, key) in post.acf_fields.bestand_om_te_downloaden" :key="key" >
-											<a v-if="item.pdf != false" class="download " :href="item.pdf"  target="_blank" v-html="item.tekst_knop"  >
-											</a>
+								<div  v-if="post.acf_fields.bestand_om_te_downloaden != false" >
+									<div class="download-parent">
+										<div style="padding-bottom: 30px">
+											<div  v-for="(item, key) in post.acf_fields.bestand_om_te_downloaden" :key="key" >
+												<a data-trigger="download-pdf" v-if="item.pdf != false" class="download " :href="item.pdf"  target="_blank" v-html="item.tekst_knop"  >
+												</a>
+											</div>
 										</div>
 									</div>
+								</div>
+
+								<div v-if="post.acf_fields.bestand_om_te_downloaden == false || post.acf_fields.fichier_a_telecharger == false " >
+									
+									<p style="padding-bottom: 30px" v-html="pll__('Plus d’infos sur la machine ci-dessous')"></p>
+
 								</div>
 								
 
@@ -552,11 +562,11 @@
 		line-height: 150%;
 	}
 
-	.download-parent div:first-child{
+/*	.download-parent div:first-child{
 
 		margin-top: 30px;
 	}
-
+*/
 	.download-parent div{
 
 		margin-top: 15px;

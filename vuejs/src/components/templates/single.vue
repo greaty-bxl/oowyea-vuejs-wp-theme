f66bff68cff68549b03c14773f73f55c2d7f888b
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-03-11 11:18:57 +0100

    Update single.vue

--- vuejs/src/components/templates/single.vue
+++ vuejs/src/components/templates/single.vue
@@ -3,13 +3,26 @@
 	<div class="section-wrap">
 		<div class="clear"></div>
 			
-			<div class="content-blog">
+			<div class="content-blog ">
+				<div class="button-rond button-rond-prev" v-if="$store.state.wp.single_prev"  >
+					<a :href="$store.state.wp.single_prev" >
+						<svg aria-hidden="true" focusable="false" role="presentation" viewBox="0 0 20 38"><path d="M4.068 18.649l15.466 16.105c.31.283.466.629.466 1.039 0 .41-.155.77-.466 1.08a1.412 1.412 0 0 1-1.038.424c-.41 0-.756-.141-1.038-.424L.466 19.708A1.373 1.373 0 0 1 0 18.648c0-.423.155-.776.466-1.059L17.458.424A1.412 1.412 0 0 1 18.496 0c.41 0 .755.141 1.038.424.31.282.466.636.466 1.06 0 .423-.155.776-.466 1.059L4.068 18.649z" fill="currentColor" fill-rule="evenodd"></path></svg>
+					 </a>	
+				</div>
+				
+				<div class="button-rond button-rond-next" v-if="$store.state.wp.single_next" >
+					<a :href="$store.state.wp.single_next" > <svg aria-hidden="true" focusable="false" role="presentation"  viewBox="0 0 20 38"><path d="M15.932 18.649L.466 2.543A1.35 1.35 0 0 1 0 1.505c0-.41.155-.77.466-1.081A1.412 1.412 0 0 1 1.504 0c.41 0 .756.141 1.038.424l16.992 17.165c.31.283.466.636.466 1.06 0 .423-.155.777-.466 1.06L2.542 36.872a1.412 1.412 0 0 1-1.038.424c-.41 0-.755-.141-1.038-.424A1.373 1.373 0 0 1 0 35.813c0-.423.155-.776.466-1.059L15.932 18.65z" fill="currentColor" fill-rule="evenodd"></path></svg> </a>
+				</div>
+				
 
 				<h1 class="title-post-blog" v-html="post.post_title" ></h1>
 				<img style="margin-top: 50px" class="image-blog-post" :src="this.post.thumb">
 				
 				<div class="clear_content_blog post" v-html="post.post_content">
 				</div>
+
+				
+
 			</div>
 		
 		<div class="clear"></div>
@@ -42,6 +55,57 @@
 </style>
 
 <style>
+	.button-rond{
+
+		display: flex;
+
+	}
+
+	.button-rond svg{
+		margin: auto;
+		height: 20px !important;
+
+	}
+
+	.button-rond a{
+
+		display: flex;
+		position: absolute;
+		height: 30px;
+		width: 30px;
+		right: 0px;
+		left: 0px;
+		background-color: #888320;
+		border: 1px solid #888320;
+		border-radius: 100%;
+		color: white;
+		transition: all .1s ease-in-out;
+	}
+
+	.button-rond a:hover{
+		color: #888320;
+		background-color: white;
+
+	}
+
+	.button-rond-next{
+
+		top: 150px;
+		right: 70px;
+		position: absolute;
+		height: 25px;
+		width: 25px;
+
+	}
+
+	.button-rond-prev{
+		top: 150px;
+		left: 70px;
+		/*right: 0px;*/
+		position: absolute;
+		height: 25px;
+		width: 25px;
+	}
 
 	.content-blog{
 
@@ -49,6 +113,7 @@
 		margin: 100px auto 100px auto;
 		box-sizing: content-box;
 		font-size: 16px;
+		position: relative;	
 
 	}
 
@@ -197,6 +262,20 @@
 			
 			width: 100%;
 			
+		}
+
+		.button-rond-next{
+
+			right: 30px;
+			top: 70px;
+
+		}
+
+		.button-rond-prev{
+
+			left: 30px;
+			top: 70px;
+		
 		}
 
 

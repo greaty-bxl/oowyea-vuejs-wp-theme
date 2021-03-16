602d9bb6e9cb78b6275d087697448d4d5ea5c104
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-03-16 11:08:43 +0100

    Update gpdr-intro.php

--- libs/gpdr-intro.php
+++ libs/gpdr-intro.php
@@ -1,5 +1,4 @@
 <?php
-//setcookie( 'owy_gpdr_accepted', 'test' );
 
 function check_gpdr_intro_accepted()
 {
@@ -10,43 +9,27 @@ function check_gpdr_intro_accepted()
 		$gpdr_accepted = true;
 	}
 	wp_vue_add_var( 'gpdr_accepted', $gpdr_accepted );
+
+	if( $_COOKIE["owy_google_accepted"] == 'true' )
+	{
+		$google_accepted = true;
+	}
+	wp_vue_add_var( 'google_accepted', $google_accepted );
 }
 add_action( 'vue_vars', 'check_gpdr_intro_accepted', 0 );
 
 
-function check_google_accepted()
-{
-	$Google_accepted = false;
-
-	// echo('<pre>');
-	// print_r($_COOKIE["owy_gpdr_accepted"]);
-	// echo('<pre>');
-	// exit();
-
-	// if( $_COOKIE["owy_gpdr_accepted"] == 'true' )
-	if( $_POST['data']['google'] == 'true' )
-		
-	{
-		?>
-		<script type="text/javascript">
-			
-		</script>
-		<?php
-		$Google_accepted = true;
-	}
-	wp_vue_add_var( 'gpdr_accepted', $gpdr_accepted );
-}
-add_action( 'wp_head', 'check_google_accepted', 0 );
-
-
 function gpdr_update_state()
 {
-	$_POST['data']['gpdr'];
-	$_POST['data']['google'];
 
 	if( $_POST['data']['gpdr'] == 'true' )
 	{
 		setcookie('owy_gpdr_accepted', 'true', time()+62208000, '/', $_SERVER['HTTP_HOST']);
+	}
+
+	if( $_POST['data']['google'] == 'true' )
+	{
+		setcookie('owy_google_accepted', 'true', time()+62208000, '/', $_SERVER['HTTP_HOST']);
 	}
 	die( json_encode( $_COOKIE ) );
 }

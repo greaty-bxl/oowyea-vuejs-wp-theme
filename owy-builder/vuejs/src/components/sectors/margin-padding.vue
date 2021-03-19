3adf28b28b265ee695a58edd5f0b49a9fca25396
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-03-19 13:07:17 +0100

    Create margin-padding.vue

---
+++ owy-builder/vuejs/src/components/sectors/margin-padding.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="sector-margin-padding" >
+		hello
+		<input type="range" class="my-input" min="10" max="50"/>
+	</div>
+</template>
+
+
+<script>
+	
+	export default{
+		
+
+	}
+
+</script>

ab4e8f49673e69870beca840248e6e117a706dfb
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-03-26 17:24:18 +0100

    Update fromagerie.php

--- libs/fromagerie/fromagerie.php
+++ libs/fromagerie/fromagerie.php
@@ -1,19 +1,19 @@
 <?php
 
-include GREATY_TEMPLATE_PATH.'/libs/fromagerie/plateau-fromage.php';
+// include GREATY_TEMPLATE_PATH.'/libs/fromagerie/plateau-fromage.php';
 
-include GREATY_TEMPLATE_PATH.'/libs/fromagerie/single-product.php';
+// include GREATY_TEMPLATE_PATH.'/libs/fromagerie/single-product.php';
 
-include GREATY_TEMPLATE_PATH.'/libs/fromagerie/les-confits.php';
+// include GREATY_TEMPLATE_PATH.'/libs/fromagerie/les-confits.php';
 
-include GREATY_TEMPLATE_PATH.'/libs/fromagerie/posts-assortiments.php';
+// include GREATY_TEMPLATE_PATH.'/libs/fromagerie/posts-assortiments.php';
 
-include GREATY_TEMPLATE_PATH.'/libs/fromagerie/assortiments-de-fromages.php';
+// include GREATY_TEMPLATE_PATH.'/libs/fromagerie/assortiments-de-fromages.php';
 
-include GREATY_TEMPLATE_PATH.'/libs/fromagerie/les-vins.php';
+// include GREATY_TEMPLATE_PATH.'/libs/fromagerie/les-vins.php';
 
-include GREATY_TEMPLATE_PATH.'/libs/fromagerie/les-pains.php';
+// include GREATY_TEMPLATE_PATH.'/libs/fromagerie/les-pains.php';
 
-include GREATY_TEMPLATE_PATH.'/libs/fromagerie/appareil.php';
+// include GREATY_TEMPLATE_PATH.'/libs/fromagerie/appareil.php';
 
-include GREATY_TEMPLATE_PATH.'/libs/fromagerie/count.php';
+// include GREATY_TEMPLATE_PATH.'/libs/fromagerie/count.php';

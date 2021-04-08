962128327739285478b6822d4f337d6a0a57eb86
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-04-08 15:37:35 +0200

    Update init-sectors.js

--- owy-builder/vuejs/src/libs/grapes/init-sectors.js
+++ owy-builder/vuejs/src/libs/grapes/init-sectors.js
@@ -1,4 +1,5 @@
 import advancedSelector from './sectors/advanced-selector.js'
+import marginPadding from './sectors/margin-padding/margin-padding.js'
 
 export default function () {
 	//let $ = this.jquery
@@ -7,5 +8,5 @@ export default function () {
 	
 	advancedSelector( this.editor, this.jquery )
 
-	
+	marginPadding( this.editor )
 }

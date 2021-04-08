5ef86027a78916bdeec6e015707f8b60413db7c8
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-04-08 09:55:03 +0200

    Update single.vue

--- vuejs/src/components/templates/single.vue
+++ vuejs/src/components/templates/single.vue
@@ -2,51 +2,32 @@
 <template>
 	<div class="section-wrap">
 		<div class="clear"></div>
+			<div class="section-global-project on-screen">
+
+				<div class="content-single">
+
+					<div class="content-project"  v-for="(item, index) in post.acf_fields.galery" :key="index">
+
+							<div v-if="index === 1 " class="desciption-tomu-project" v-html="post.post_content"></div>
+							<span v-else>
+								
+							<img v-if="is" :src="item">	
+							<div v-else class="background-project" :style= "{'background-image': 'url('+item+')' }" >
+								
+							</div>
+
+							</span>
+		
+					</div>
 			
-			<div class="content-single">
-				<div>
-					<img class="image-single-post" :src="this.post.thumb">
+						<div class="retour">
+							
+							<button onclick="window.history.back()">Retour</button>
+
+						</div>
+				
 				</div>
-				<div class="single-text-post "> 
-					<h2 class="montserrat-bold title-single-fromagerie" v-html="post.post_title">Colis</h2>
 
-					<div v-html="this.post.acf_fields.list_colis" class="list-colis">
-					</div>
-					<div>				
-						<!-- <p v-html="this.post.metas._regular_price +' €' " class="price-single montserrat-bold"> </p> -->
-					</div>
-
-					<div>				
-					<!-- 	<input type="number"  name=""> -->
-						
-					</div>			
-
-					<h2 class="montserrat-bold title-single-fromagerie-1">Commandes et demandes spéciales</h2>
-					
-					<div class="contener-single">
-						<form action="" method="get" class="form-example"  >
-							<input class="input-single-title"    type="" name="">
-							<input class="input-single"  placeholder="Prénom"  type="" name="">
-							<input class="input-single" placeholder="E-mail" type="" name="">
-							<input class="input-single" placeholder="Téléphone" type="" name="">
-							<input class="input-single" placeholder="Nombre de personnes" type="" name="">
-							<input class="input-single title-21 " placeholder="Assortiments en plat ou dessert " disabled type="" name="">
-							<textarea class="input-single " placeholder="Messsage"  type="" name=""></textarea>
-							<button class="button-envoyer">Envoyer</button>
-						</form>
-					</div>
-
-					<div class="retour">
-						
-						<button onclick="window.history.back()">Retour</button>
-
-					</div>
-					
-			
-
-
-					
-				</div>
 			</div>
 		
 		<div class="clear"></div>
@@ -57,7 +38,18 @@
 <script>
 
 	import smart_fonts from 'Libs/smart-fonts.js';
+	import is from 'is_js'
 	export default {
+
+		data(){
+
+			// currentRoute: window.location.pathname
+			return {
+				is : is.mobile(),
+				// showButton : window.location.toString()
+			}
+		},
+
 		components: {
 			
 		},
@@ -68,11 +60,23 @@
 			var $ = this.$
 
 			// $('.retour').html('[add_to_cart id="99"]')
+			console.log(this.post);
 
 			
 
 			$('.title-21').attr('placeholder', this.post.post_title );
 			$('.title-21').attr('value', this.post.post_title );
+
+				$('.section-global-project').on('enter-screen', () => {
+			console.log('enter-screen');
+
+			$('.header-tomu-container').css('color', '#BBB5D6');
+			$('.current-page').css('color', '#BBB5D6');
+			// $('.title-filter:contains("Origines")').text('Afkomst')
+			
+			
+		})
+
 
 			
 			this.$emit('template_mounted')
@@ -92,479 +96,499 @@
 <style>
 
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400&display=swap');
-	.input-single-title{
-
-		display: none;
-	}
-	#header{
-
-		height: 0px;
-	}
-	.bancontact{
-
-		height: 40px !important;
-	}
-
-	.list-colis{
-		padding-top: 0px !important;
-	}
-
-	.list-colis p {
-		padding-top: 16px !important;
-	}
 	
-	
-	.button-contener{
-
-		display: flex;
-		flex-direction: column;
-
-	}
-	.button-contener a{
-
-		width: 100%;
-		
-		padding: 20px 0px !important;
-	}
-
-	.button-contener p{
-		padding: 0px !important;
-		margin-top: 15px;
-		/*background-color: #A59890;*/
-		color: white;
-		border-width: 0 ;
-		border:0px !important ;
-		text-align: center;
-
-	}
-
-	.button-envoyer{
-
-		text-align: center !important;
-		padding-top: 20px !important;
-		padding-bottom: 20px !important;
-		background-color: #70625B !important;
-		width: 100%;
-		font-size: 16px !important;
-	}
-
-	.button-envoyer:hover{
-
-		text-align: center !important;
-		padding-top: 20px !important;
-		padding-bottom: 20px !important;
-		background-color: white !important;
-		color: #70625B !important;
-		width: 100%
-	}
-
-
-
-
-/*	.button-contener button{
-
-		background-color: #70625B;
-		color: white;
-		border-width: 0 ;
-	}*/
-
-
-/*	.button-contener button{
-
-		text-align: center;
-		padding: 20px 0px;
-
-	}*/
-	
-	.content-single{
-
-		display: flex;
-		flex-direction: row;
-		margin-top: 150px;
-		margin-bottom: 50px;
-		margin-right: auto;
-		margin-left: auto;
-
-		width: 60vw;
-
-	}
-	.image-single-post{
-		min-width: auto !important;
-		width: 40vw;
-	}
-
-	.single-text-post  p{
-
-		margin: 0px;
-		font-weight: 400 !important;
-		line-height: 24px;
-		
-		/*padding-top: 16px;*/
-	}
-.single-text-post li{
-
-	font-weight: 400 !important;
-
-}
-
-	.single-text-post  div{
-
-		/*margin: 0px;*/
-		padding-top: 16px;
-
-	}
-
-	.single-text-post {
-
-		/*height: 30vw !important;*/
-		/*width: 30vw;*/
-		padding-left: 5vh;
-	}
-	.single-text-post  p {
-
-		font-size: 16px !important;
-		font-weight: 500;
-
-	}
-
-	.single-text-post  li {
-
-		font-size: 16px !important;
-		font-weight: 500;
-	}
-
-	.content-single ul li{
-
-		list-style: none;
-
-	}
-	.content-single ul{
-
-		padding-left: 0;
-	}
-
-	.content-single textarea{
-		
-		min-width: 20vw;
-	}
-
-
-	.content-single *{
-
-		text-align: left;
-		color: #4D4D4D;
-		
-	}
-
-	.montserrat-bold{
-
-		font-family: 'Montserrat', sans-serif !important;
-		font-weight: bold !important;
-		color: #70625B;
-	}
-
-	.price-single{
-
-		font-size: 20px !important;
-	}
-
-	.title-single-fromagerie{
-
-		font-size: 25px !important;
-	}
-	.title-single-fromagerie-1{
-
-		font-size: 20px !important;
-	}
-
-	.content-single input{
-		margin-top: 5px;
-		height: 30px;
-		width: 20vw;
-	}
-
-	.single-text-post  div p:nth-child(2){
-
-		padding-top: 10px;
-	}
-
-	.single-text-post  ul li{
-
-		padding-top: 3px;
-	}
-
-	.nbrpersonne{
-
-		padding-bottom: 10px !important;
-	}
-
-	.retour{
-
-		padding-top: 30px !important;
-		color: #A59890 !important;
-	}
-
-	.retour button{
-		font-size: 16px !important;
-		border: none !important;
-		background-color: white !important;
-		text-decoration-line: none !important;
-		font-weight: 400 !important;
-		color: #70625B !important;
-		padding-left: 0px !important;
-		margin-top: 0px !important;
-
-	}
-	
-
-
-	@media screen and (max-width: 1100px) and (min-width: 600px ) {
-
-		.content-single{
-
-			display: flex;
-			flex-direction: column;
-			margin-top: 150px;
-			margin-bottom: 150px;
-			margin-right: 70px;
-			margin-left: 70px;
-
-		}
-
-		.content-single input{
-		
-			width: auto;
-			font-size: 18px !important;
-		}
-		.content-single textarea{
-		
-			font-size: 18px !important;
-		}
-
-		.single-text-post {
-
-		padding-left: 0;
-		padding-top: 5vh;
-
-		}
-
-		.image-single-post {
-			/*height: 60vw;*/
-			min-width: 600px !important;
-			width: 60vw !important;
-		}
-
-		.single-text-post  div{
-
-
-			padding-top: 20px;
-		}
-		.button-contener p{
-
-			width: 50%;
-			/*padding: 10px 0px;*/
-
-		}
-
-		.single-text p{
-
-			line-height: 24px !important;
-
-		}
-	}
-
-	@media screen and (max-width: 600px){
-
-		.content-single{
-
-			display: flex;
-			flex-direction: column;
-			padding-top: 0vh;
-			margin-bottom: 50px;
-			margin-top: 130px;
-			margin-left: 30px;
-			margin-right: 30px;
-			width: auto;
-		}
-
-		.content-single input{
-		
-			width: auto;
-			font-size: 15px !important;
-		}
-
-		.content-single textarea{
-
-			font-size: 15px !important;
-		}
-
-		.single-text-post {
-
-			padding-left: 0;
-			padding-top: 2.5vh;
-
-		}
-		.image-single-post {
-			min-width: auto !important;
-			width: calc(100vw - 60px) !important;
-
-		}
-
-		.button-contener p{
-
-			width: 50%;
-			/*padding: 10px 0px;*/
-
-		}
-		.button-contener a{
-
-			width: 100%;
-			/*padding: 10px 0px;*/
-
-		}
-
-		.single-text p{
-
-			line-height: 20px !important;
-
-		}
-
-		.woocommerce .quantity .qty{
-
-			width: 70px !important;
-		}
-
-	}
-
 	input:focus {
 	color: #70625b !important;
 	}
+
+		.desciption-tomu-project{
+
+			/*height: 100%;*/
+			margin-top: auto;
+			margin-bottom: auto;
+			display: flex;
+			justify-content: center;
+			
+			
+
+		}
+
+		.desciption-tomu-project p{
+
+			height: auto;
+			font-size: 16px;
+			text-align: left;
+			font-weight: 700;
+
+		}
+
+
+		.content-project{
+
+			position: relative;
+			/*overflow: hidden;*/
+		}
+
+		.animation-title *{
+
+			all: unset;
+		}
+		.animation-title{
+
+			height: 100%;
+			position: absolute;
+			width: 100%;
+			left: 0;
+			top: 0;
+			background-color: #ffffffb0;
+			display: flex;
+			align-items: start;
+			opacity:0;
+			-webkit-transition:all 0.45s ease-in-out;
+			-moz-transition:all 0.45s ease-in-out;
+			-o-transition:all 0.45s ease-in-out;
+			-ms-transition:all 0.45s ease-in-out;
+			transition:all 0.45s ease-in-out;
+			color: black;
+			font-size: 25px; 
+		
+		}
+
+		.content-project a{
+
+			text-decoration: none;
+		}
+
+		.animation-title p{
+
+			width: 50%;
+			margin-left: auto;
+			margin-right: 20px;
+			text-align: right;
+			font-weight: 500;
+			padding-top: 20px;
+
+		}
+
+		.background-project{
+
+			height: 100%;
+			width: 100%;
+			background: no-repeat;
+			background-size: cover;
+			background-position: center;
+			-webkit-transition:all 0.25s ease-in-out;
+			-moz-transition:all 0.25s ease-in-out;
+			-o-transition:all 0.25s ease-in-out;
+			-ms-transition:all 0.25s ease-in-out;
+			transition:all 0.25s ease-in-out;
+			/*-webkit-filter: grayscale(100%) contrast(120%);*/
+			/*filter: grayscale(100%) contrast(120%);*/
+
+
+		}
+
+		.section-global-project{
+
+			width: calc( 100% - 245px );
+			position: relative;
+		}
+		
+		.content-project:nth-child(odd) { 
+
+			/*display: flex;*/
+			justify-content: flex-end;
+
+		}
+
+
+
+		.section-global-project {
+
+			padding-top: 100px;
+			padding-bottom: 100px;
+			box-sizing: border-box;
+			padding-left: 70px;
+		}
+
+		.content-project:nth-child(16n+1){
+
+			width: 30vw;
+			height: 40vw;
+		}	
+
+		.content-project:nth-child(16n+2){
+			width: 30vw;
+			margin-top: -22.5vw;
+			height: 40vw;
+			margin-left : calc( 30vw + 70px );
+			margin-bottom: 70px;
+			display: flex;
+		}
+
+		.content-project:nth-child(16n+3){
+
+			margin-top: -17.5vw;
+			margin-bottom: 100px;
+			width: 30vw;
+			height: 25vw;
+		}
+
+		.content-project:nth-child(16n+4){
+
+			width: 47vw;
+			height: 33vw;
+			margin-left: auto;
+			margin-bottom: 70px;
+
+		}
+
+		.content-project:nth-child(16n+5){
+
+			width: 30vw;
+			height: 40vw;
+			margin-left: auto;
+
+		}
+
+		.content-project:nth-child(16n+6){
+
+			width: 35vw;
+			height: 22.5vw;
+			margin-right: 70px;
+			margin-left: calc( 100% - 65vw - 70px );
+			margin-top: -40vw;
+
+		}
+
+		.content-project:nth-child(16n+7){
+
+			width: calc( 50vw - 140px)  ;
+			height: 50vw;
+			margin-right: 70px;
+			margin-top: calc(  17.5vw + 100px );
+			box-sizing: content-box;
+
+		}
+
+		.content-project:nth-child(16n+8){
+
+			width: 25vw;
+			height: 35vw;
+			margin-right: 70px;
+			margin-top: -35vw;
+			margin-bottom: 70px;
+			box-sizing: content-box;
+			margin-left: calc( 50vw - 70px);
+
+		}
+
+		.content-project:nth-child(16n+9){
+
+			width: 25vw;
+			height: 35vw;
+			margin-right: 70px;
+			/*margin-top: -35vw;*/
+			box-sizing: content-box;
+			margin-left: calc( 37.5vw - 70px);
+
+		}
+
+		.content-project:nth-child(16n+10){
+
+			width: 30vw;
+			margin-top: 100px;
+			height: 25vw;
+			
+		}	
+
+		.content-project:nth-child(16n+11){
+
+			width: calc( 100% - 30vw - 70px );
+			margin-top: -12.5vw;
+			height: 40vw;	
+			margin-left : calc( 30vw + 70px );
+			margin-bottom: 70px;
+		}
+
+		.content-project:nth-child(16n+12){
+
+			margin-top: -27.5vw;
+			margin-bottom: 70px;
+
+			width: 30vw;
+			/*margin-left: 70px;*/
+			height: 40vw;
+		}
+
+		.content-project:nth-child(16n+13){
+
+			width: calc( 100% - 30vw - 70px );
+			height: 50vw;
+			margin-left: auto;
+			/*margin-top: calc(  17.5vw + 100px );*/
+			box-sizing: content-box;
+
+		}
+
+		.content-project:nth-child(16n+14){
+
+			width: 30vw;
+			margin-top: 100px;
+			height: 25vw;
+
+		}
+
+		.content-project:nth-child(16n+15){
+		
+			width: 30vw;
+			margin-top: -25vw;
+			margin-bottom: 70px;
+			height: 40vw;
+			margin-left : calc( 30vw + 70px );
+
+		}
+
+		.content-project:nth-child(16n+16){
+		
+			width: 30vw;
+			/*margin-left: 70px;*/
+			height: 33vw;
+			margin-left : calc( 30vw + 70px );
+			width: calc( 100% - 30vw - 70px );
+
+		}
+
+	/*	.section-global-project div{
+
+			display: flex;
+			flex-direction: column;
+		}
+	*/
+		.section-global-project img{
+
+			height: 50px;
+			width: 50px;
+		}
+
+		@media screen and ( max-width: 1125px ) and ( min-width: 600px){
+
+
+			.section-global-project {
+				width: calc( 100% - 180px );
+				padding-left: 50px;
+			}
+
+
+
+			.content-project:nth-child(16n+1){
+
+				width: 30vw;
+				height: 40vw;
+			}	
+
+			.content-project:nth-child(16n+2){
+				width: 30vw;
+				margin-top: -22.5vw;
+				height: 40vw;
+				margin-left : calc( 30vw + 30px );
+				margin-bottom: 30px;
+			}
+
+			.content-project:nth-child(16n+3){
+
+				margin-top: -17.5vw;
+				margin-bottom: 30px;
+				width: 30vw;
+				height: 25vw;
+			}
+
+			.content-project:nth-child(16n+4){
+
+				width: 47vw;
+				height: 33vw;
+				margin-left: auto;
+				margin-bottom: 30px;
+
+			}
+
+			.content-project:nth-child(16n+5){
+
+				width: 30vw;
+				height: 40vw;
+				margin-left: auto;
+
+			}
+
+			.content-project:nth-child(16n+6){
+
+				width: 35vw;
+				height: 22.5vw;
+				margin-right: 30px;
+				margin-left: calc( 100% - 65vw - 30px );
+				margin-top: -40vw;
+
+			}
+
+			.content-project:nth-child(16n+7){
+
+				width: calc( 43.9vw - 30px);
+				height: 50vw;
+				margin-right: 30px;
+				margin-top: calc(  17.5vw + 100px );
+				box-sizing: content-box;
+
+			}
+
+			.content-project:nth-child(16n+8){
+
+				width: 25vw;
+				height: 35vw;
+				margin-right: 30px;
+				margin-top: -35vw;
+				margin-bottom: 30px;
+				box-sizing: content-box;
+				margin-left: 43.9vw;
+
+			}
+
+			.content-project:nth-child(16n+9){
+
+				width: 25vw;
+				height: 35vw;
+				margin-right: 30px;
+				box-sizing: content-box;
+				margin-left: calc( 37.5vw - 30px);
+
+			}
+
+			.content-project:nth-child(16n+10){
+
+				width: 30vw;
+				margin-top: 100px;
+				height: 25vw;
+				
+			}	
+
+			.content-project:nth-child(16n+11){
+
+				width: calc( 100% - 30vw - 30px );
+				margin-top: -12.5vw;
+				height: 40vw;	
+				margin-left : calc( 30vw + 30px );
+				margin-bottom: 30px;
+			}
+
+			.content-project:nth-child(16n+12){
+
+				margin-top: -27.5vw;
+				margin-bottom: 30px;
+
+				width: 30vw;
+				/*margin-left: 30px;*/
+				height: 40vw;
+			}
+
+			.content-project:nth-child(16n+13){
+
+				width: calc( 100% - 30vw - 30px );
+				height: 50vw;
+				margin-left: auto;
+				/*margin-top: calc(  17.5vw + 100px );*/
+				box-sizing: content-box;
+
+			}
+
+			.content-project:nth-child(16n+14){
+
+				width: 30vw;
+				margin-top: 100px;
+				height: 25vw;
+
+			}
+
+			.content-project:nth-child(16n+15){
+			
+				width: 30vw;
+				margin-top: -25vw;
+				margin-bottom: 30px;
+				height: 40vw;
+				margin-left : calc( 30vw + 30px );
+
+			}
+
+			.content-project:nth-child(16n+16){
+			
+				width: 30vw;
+				/*margin-left: 30px;*/
+				height: 33vw;
+				margin-left : calc( 30vw + 30px );
+				width: calc( 100% - 30vw - 30px );
+
+			}
+
+			
+		}
+
+		@media screen and ( max-width: 600px ){
+
+			.section-global-project img{
+
+				width: 100% !important;
+				height: auto !important;
+
+			}
+
+			.section-global-project{
+
+				width: calc( 100% - 60px );
+				padding-left: 0px;
+				margin-left: auto;
+				margin-right: auto;
+			}
+
+			.content-project{
+
+				width: 100% !important;
+				margin: 0px 0px 15px 0px !important;
+				height: auto !important;
+			}
+
+			.animation-title {
+				font-size: 18px ;
+				padding: 15px 0px;
+				height: auto;
+				display: block;
+				position: initial;
+				width: 100%;
+				left: 0;
+				top: 0;
+				background-color: white;
+				display: flex;
+				align-items: center;
+				opacity: 1;
+				transition: all 0.45s ease-in-out;
+				color: black;
+
+			}
+
+			.animation-title p{
+
+				padding-top: 0px;
+				width: calc( 100% - 60px  );
+				margin-left: auto;
+				text-align: right;
+
+			}
+
+		}
+
 
 
 
 </style>
 
 <style scoped>
-
-	.woocommerce #respond input#submit, .woocommerce a.button, .woocommerce button.button, .woocommerce input.button{
-
-		text-align: center;
-	}
-
-	.content-single del{
-
-		display: none !important;
-	}
-
-	.content-single bdi {
-
-		display: none !important;
-	}
-
-	a{
-		text-decoration-line: none;
-		color: #666666;
-	}
-
-	input{
-
-		border: 1px solid #A59890 !important;
-		margin-top: 15px;
-		padding-top: 10px;
-		padding-bottom: 10px;
-		color: #666666;
-		padding-left: 15px;
-
-	}
-
-	textarea{
-		padding-top: 10px;
-		border: 1px solid #A59890 !important;
-		margin-top: 15px;
-		min-height: 100px;
-		padding-left: 15px;
-	}
-
-	button{
-
-		margin-top: 20px;
-		padding-top: 10px;
-		padding-bottom: 10px;
-		background: #70625b;
-		color: white;
-		border: solid #70625b;
-		border-width: 1px;
-		font-weight: bold;
-
-	}
-
-	button:hover{
-
-		color: #A59890;
-		background: white;
-		/*border: 1px ;*/
-	/*	border: solid #A59890;
-		border-width: 1px;*/
-		transition-delay: 200;
-	}
-
-	.contener-single{
-		padding-top: 0px;
-	}
-
-	@media screen and (max-width: 1100px) and (min-width: 600px){
-
-		h5{
-
-			font-size: 26px !important;
-		}
-	}
-
-	@media screen and (max-width: 600px){
-
-
-	}
-
-	.contener-single form{
-
-		display: flex;
-		flex-direction: column;
-		min-width: 20vw;
-		/*justify-content: space-between;*/
-	}
-	
-
-
-	@media screen and (max-width: 1100px) and (min-width: 600px){
-
-
-		.contener-single form{
-
-			max-width: 80% !important;
-			/*justify-content: space-between;*/
-		}
-
-		p{
-
-			font-size: 18px !important;
-		}
-
-
-
-	}
-
-	@media screen and (max-width: 600px){
-
-
-		.contener-single form{
-
-			max-width: 100%;
-			/*justify-content: space-between;*/
-		}
-
-	}
-
-
 
 </style>
 

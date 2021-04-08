4bd80d60bb2943320bc5bb6de7b4550d2a385c08
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-04-08 09:55:11 +0200

    Create footer-tomu.vue

---
+++ vuejs/src/components/organisms/footer-tomu.vue
@@ -0,0 +1,46 @@
+<template>
+
+		<div id="footer" class="footer-tomu">
+		<p><span id='year'></span> TOMU studio / GREATY</p>
+		</div>
+
+</template>
+
+<script type="text/javascript">
+	
+	export default{
+
+		mounted(){
+
+			document.getElementById("year").innerHTML = new Date().getFullYear();
+
+			let $ = this.$
+
+			$('.contact-tomu div').on('enter-screen', () => {
+					console.log('enter-screen');
+					$('.header-tomu-container').css('color', '#fdd498');
+					$('.current-menu-item a').css('color', '#fdd498');
+					
+			})
+		}
+	}
+
+
+</script>
+
+<style type="text/css">
+	
+	.footer-tomu{
+		text-align: right;
+		padding-right: 70px;
+		margin-top: -50px;
+		font-size: 16px;
+	}
+	@media  screen and ( max-width: 700px ){
+
+		.footer-tomu{
+			padding-right: 30px;;
+		}
+	}
+
+</style>

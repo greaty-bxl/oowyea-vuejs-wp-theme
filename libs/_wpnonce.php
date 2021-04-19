cf496c0ef7f5c29993bccbbbf77124edc184ff36
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-04-19 18:31:37 +0200

    Update _wpnonce.php

--- libs/_wpnonce.php
+++ libs/_wpnonce.php
@@ -1,14 +1,14 @@
 <?php
 
-function wp_redirect_filter($status, $location) {
-    if( $_GET['add_to_json'] && $_GET['_wpnonce'] )
-	{
-		echo "refresh";
-		exit();
-	}
-	if( is_admin() )
-	{
-		return $status;	
-	}	
-}
-add_filter( 'wp_redirect_status', 'wp_redirect_filter', 10, 2);
+// function wp_redirect_filter($status, $location) {
+//     if( $_GET['add_to_json'] && $_GET['_wpnonce'] )
+// 	{
+// 		echo "refresh";
+// 		exit();
+// 	}
+// 	if( is_admin() )
+// 	{
+// 		return $status;	
+// 	}	
+// }
+// add_filter( 'wp_redirect_status', 'wp_redirect_filter', 10, 2);

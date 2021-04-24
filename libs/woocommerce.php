480adcfee22390244a139b77934431fffe49e716
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-04-24 19:26:31 +0200

    Update woocommerce.php

--- libs/woocommerce.php
+++ libs/woocommerce.php
@@ -1,6 +1,6 @@
 <?php
-include 'woocommerce/remove-scripts.php';
-include 'woocommerce/hierarchy.php';
-include 'woocommerce/shortcode.php';
-include 'woocommerce/return-to-shop.php';
-include 'woocommerce/ajax_forms.php';
+// include 'woocommerce/remove-scripts.php';
+// include 'woocommerce/hierarchy.php';
+// include 'woocommerce/shortcode.php';
+// include 'woocommerce/return-to-shop.php';
+// include 'woocommerce/ajax_forms.php';

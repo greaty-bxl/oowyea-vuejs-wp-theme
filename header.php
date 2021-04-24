9c9f42572eee81d2c3c2266b17d1812623a0b46a
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-04-24 19:26:24 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php 
 do_action( 'before_document' );
 
-$previous_url = $_SERVER['HTTP_REFERER'];
+// $previous_url = $_SERVER['HTTP_REFERER'];
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 ?>
 <!doctype html>

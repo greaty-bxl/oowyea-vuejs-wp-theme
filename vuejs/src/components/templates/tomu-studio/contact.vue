57af9a3ab5f1876eaf336072d2bb6b9313da7042
salim <61011295+salim-rhachak@users.noreply.github.com>
2021-05-07 10:23:21 +0200

    Update contact.vue

--- vuejs/src/components/templates/tomu-studio/contact.vue
+++ vuejs/src/components/templates/tomu-studio/contact.vue
@@ -4,10 +4,10 @@
 
 			<div class="on-screen">
 				<ul>
-					<li><a target="_blank" href="mailto: HELLO@TOMUSTUDIO.COM">HELLO@TOMUSTUDIO.COM</a></li>
+					<li style="margin-bottom: 20px;"><a target="_blank" href="mailto: HELLO@TOMUSTUDIO.COM">HELLO@TOMUSTUDIO.COM</a></li>
 					<li> <a target="_blank" href="https://www.google.com/maps/place/Avenue+Brugmann+16,+1060+Saint-Gilles/@50.8217959,4.3512311,17z/data=!3m1!4b1!4m5!3m4!1s0x47c3c458b350e9cb:0x1e6f59366739416!8m2!3d50.8217959!4d4.3534198">AVENUE BRUGMANN 16</a></li>
 					<li> <a target="_blank" href="https://www.google.com/maps/place/Avenue+Brugmann+16,+1060+Saint-Gilles/@50.8217959,4.3512311,17z/data=!3m1!4b1!4m5!3m4!1s0x47c3c458b350e9cb:0x1e6f59366739416!8m2!3d50.8217959!4d4.3534198">1060 BRUSSELS</a></li>
-					<li><a target="_blank" href="tel:+32 2 897 15 33">+32 2 897 15 33</a></li>
+					<li style="margin-top: 20px;"><a target="_blank" href="tel:+32 2 897 15 33">+32 2 897 15 33</a></li>
 					
 				</ul>
 
@@ -29,7 +29,7 @@
 			
 			$('[data-state="current"] .contact-tomu div').on('enter-screen', () => {
 				console.log('enter-screen');
-				$('.header-tomu-container').css('color', '#BBD7B3');
+				// $('.header-tomu-container').css('color', '#BBD7B3');
 				$('.header-tomu-container svg').css('color', '#BBD7B3');
 				$('.header-tomu-container a').css('color', 'black');
 
